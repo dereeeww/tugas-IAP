e0062a1618cfe54e8d28cd229d21cd2fd6659b3a
dereeeww <dreolinasari@gmail.com>
2025-06-01 14:07:13 +0700

    berhasil cari pakai enter

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -1,4 +1,4 @@
-$('#search-button').on('click', function () {
+function searchMovie() {
     $.ajax({
         url: 'https://www.omdbapi.com/',
         type: 'get',
@@ -10,7 +10,7 @@ $('#search-button').on('click', function () {
         success: function (result) {
             if (result.Response == "True") {
                 let movie = result.Search;
-                $('#movie-list').html(''); 
+                $('#movie-list').html('');
 
                 $.each(movie, function (i, data) {
                     $('#movie-list').append(`
@@ -18,7 +18,7 @@ $('#search-button').on('click', function () {
                             <div class="card">
                                 <img src="${data.Poster}" class="card-img-top" alt="${data.Title}">
                                 <div class="card-body">
-                                    <h5 class="Judul Film">${data.Title}</h5>
+                                    <h5 class="card-title">${data.Title}</h5>
                                     <h6 class="card-subtitle mb-2 text-muted">${data.Year}</h6>
                                     <a href="#" class="btn btn-primary">See Details</a>
                                 </div>
@@ -38,4 +38,16 @@ $('#search-button').on('click', function () {
             }
         }
     });
+}
+
+// Saat tombol diklik
+$('#search-button').on('click', function () {
+    searchMovie();
+});
+
+// Saat tombol ENTER ditekan pada input
+$('#search-input').on('keyup', function (e) {
+    if (e.keyCode === 13) {
+        searchMovie();
+    }
 });

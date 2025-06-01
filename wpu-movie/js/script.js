2decc5733575848e9f89582de4110627b538a12a
dereeeww <dreolinasari@gmail.com>
2025-06-01 13:45:58 +0700

    berhasil card movie

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -1,6 +1,6 @@
 $('#search-button').on('click', function () {
     $.ajax({
-        url: 'https://omdbapi.com/',
+        url: 'https://www.omdbapi.com/',
         type: 'get',
         dataType: 'json',
         data: {
@@ -9,13 +9,28 @@ $('#search-button').on('click', function () {
         },
         success: function (result) {
             if (result.Response == "True") {
-             let movie = result.Search;
-             console.log(movie);
+                let movie = result.Search;
+                $('#movie-list').html(''); // Kosongkan dulu daftar sebelumnya
+
+                $.each(movie, function (i, data) {
+                    $('#movie-list').append(`
+                        <div class="col-md-4 mb-3">
+                            <div class="card">
+                                <img src="${data.Poster}" class="card-img-top" alt="${data.Title}">
+                                <div class="card-body">
+                                    <h5 class="card-title">${data.Title}</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">${data.Year}</h6>
+                                    <a href="#" class="btn btn-primary">Show Details</a>
+                                </div>
+                            </div>
+                        </div>
+                    `);
+                });
 
             } else {
                 $('#movie-list').html(`
                     <div class="col">
-                        <h1 class="text-center">` + result.Error + `</h1>
+                        <h1 class="text-center">${result.Error}</h1>
                     </div>
                 `);
             }
